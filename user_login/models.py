e4b02f0be3f50d7dffa222aa8cfd054ebbd6668d
Haryohmi <mahyorfrancis@gmail.com>
2024-04-01 02:19:09 +0100

    user reg & login

---
+++ user_login/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('You must provide an email address')
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **kwargs):
+        user = self.create_user(email, password=password, **kwargs)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField(unique=True, db_index=True)
+    age = models.IntegerField()
+    location = models.TextField()
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name + ' ' + self.email
+    
+        
+        
